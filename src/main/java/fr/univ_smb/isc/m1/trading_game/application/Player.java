fc346fd53b171454478b8470eb4c3dbd2f8443a0
= <nils.ruet@etu.univ-smb.fr>
2021-04-06 16:28:57 +0200

    feat: created methods to create orders for a player

--- src/main/java/fr/univ_smb/isc/m1/trading_game/application/Player.java
+++ src/main/java/fr/univ_smb/isc/m1/trading_game/application/Player.java
@@ -1,18 +1,38 @@
 package fr.univ_smb.isc.m1.trading_game.application;
 
+import fr.univ_smb.isc.m1.trading_game.infrastructure.persistence.Ticker;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
     private Object user;//TODO user class instead of Object
     private List<Portfolio> portfolios;
+    private List<Order> orders;
 
     public Player(Object user, int portfolioCount, int initialBalance) {
         this.user = user;
         this.portfolios = new ArrayList<>();
+        this.orders = new ArrayList<>();
         for(int i=0; i<portfolioCount; i++){
             Portfolio port = new Portfolio(initialBalance);
             portfolios.add(port);
         }
     }
+
+    public void createBuyOrder(int portfolioNumber, Ticker ticker, int quantity) {
+        if (portfolioNumber < portfolios.size() && portfolioNumber > 0) {
+            BuyOrder o = new BuyOrder(portfolios.get(portfolioNumber), ticker, quantity);
+            orders.add(o);
+        }
+    }
+    
+    public void createSellOrder(int portfolioNumber, Ticker ticker, int quantity){
+        if (portfolioNumber < portfolios.size() && portfolioNumber > 0) {
+            SellOrder o = new SellOrder(portfolios.get(portfolioNumber), ticker, quantity);
+            orders.add(o);
+        }
+    }
+
+
 }
