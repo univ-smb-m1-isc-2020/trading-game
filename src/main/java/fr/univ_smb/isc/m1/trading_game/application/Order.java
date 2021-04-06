976da13b52591185f819c0b2629568de9daaf851
= <nils.ruet@etu.univ-smb.fr>
2021-04-06 15:48:56 +0200

    Introduced abstract class Order and two future implementations BuyOrder and SellOrder

---
+++ src/main/java/fr/univ_smb/isc/m1/trading_game/application/Order.java
@@ -0,0 +1,18 @@
+package fr.univ_smb.isc.m1.trading_game.application;
+
+import fr.univ_smb.isc.m1.trading_game.infrastructure.persistence.Ticker;
+
+public abstract class Order {
+    
+    protected Portfolio portfolio;
+    protected Ticker ticker;
+    protected int quantity;
+
+    public Order(Portfolio portfolio, Ticker ticker, int quantity) {
+        this.portfolio = portfolio;
+        this.ticker = ticker;
+        this.quantity = quantity;
+    }
+
+    public abstract void apply();
+}
---
+++ src/main/java/fr/univ_smb/isc/m1/trading_game/application/SellOrder.java
@@ -0,0 +1,15 @@
+package fr.univ_smb.isc.m1.trading_game.application;
+
+import fr.univ_smb.isc.m1.trading_game.infrastructure.persistence.Ticker;
+
+public class SellOrder extends Order{
+
+    public SellOrder(Portfolio portfolio, Ticker ticker, int quantity) {
+        super(portfolio, ticker, quantity);
+    }
+
+    @Override
+    public void apply() {
+
+    }
+}
