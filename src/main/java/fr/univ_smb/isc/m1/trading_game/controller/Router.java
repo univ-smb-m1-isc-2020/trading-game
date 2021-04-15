7f442e201c94b296357cb6cb205976e94af48f2b
Xavier-Nourry <xavier.nourry@etu.univ-savoie.fr>
2021-04-15 12:05:09 +0200

    feat: added createOrder page with button link + routes re-organisation

--- src/main/java/fr/univ_smb/isc/m1/trading_game/controller/Router.java
+++ src/main/java/fr/univ_smb/isc/m1/trading_game/controller/Router.java
@@ -1,14 +1,22 @@
 package fr.univ_smb.isc.m1.trading_game.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class gameManager {
+public class Router {
     @RequestMapping(value = { "/", "/index" })
     public String index() {
+        return "homePageCommon";
+    }
+
+    @RequestMapping(value = "/gameManager")
+    public String gameManager() {
         return "gameManager";
+    }
+
+    @RequestMapping(value = "/createOrder")
+    public String createOrder() {
+        return "createOrder";
     }
 }
