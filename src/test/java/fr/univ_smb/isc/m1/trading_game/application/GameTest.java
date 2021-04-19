5f5744d0075e22017382918808a4ae384243c087
= <nils.ruet@etu.univ-smb.fr>
2021-04-19 09:44:15 +0200

    chore: created GameTest class with empty tests

---
+++ src/test/java/fr/univ_smb/isc/m1/trading_game/application/GameTest.java
@@ -0,0 +1,66 @@
+package fr.univ_smb.isc.m1.trading_game.application;
+
+import org.junit.jupiter.api.Test;
+
+public class GameTest {
+
+    @Test
+    public void getCurrentDuration(){
+
+    }
+
+    @Test
+    public void getTotalDuration(){
+
+    }
+
+    @Test
+    public void getStartDate(){
+
+    }
+
+    @Test
+    public void getPortfolioCount(){
+
+    }
+
+    @Test
+    public void getInitialBalance(){
+
+    }
+
+    @Test
+    public void getTransactionFee(){
+
+    }
+
+    @Test
+    public void getPlayerCount(){
+
+    }
+
+    @Test
+    public void getRankings(){
+
+    }
+
+    @Test
+    public void createPlayer(){
+
+    }
+
+    @Test
+    public void applyEod(){
+
+    }
+
+    @Test
+    public void hasEnded(){
+
+    }
+
+    @Test
+    public void hasNotEnded(){
+
+    }
+}
