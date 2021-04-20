49fd9a1705a49eb8638ad2f128a9e3cc54fd7d24
= <nils.ruet@etu.univ-smb.fr>
2021-04-20 11:27:53 +0200

    feat: GameScheduler can now make the game advance one turn per day

--- src/main/java/fr/univ_smb/isc/m1/trading_game/application/Game.java
+++ src/main/java/fr/univ_smb/isc/m1/trading_game/application/Game.java
@@ -1,8 +1,6 @@
 package fr.univ_smb.isc.m1.trading_game.application;
 
 import fr.univ_smb.isc.m1.trading_game.infrastructure.persistence.EOD;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.support.CronTrigger;
 
 import java.util.*;
 
