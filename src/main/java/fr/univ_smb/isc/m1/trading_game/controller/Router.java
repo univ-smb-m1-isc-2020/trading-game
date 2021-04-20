29c1248b1fb233b62c4e220898cb274c7383a611
Xavier-Nourry <xavier.nourry@etu.univ-savoie.fr>
2021-04-20 10:25:48 +0200

    feat: added gameManager and createOrder views

--- src/main/java/fr/univ_smb/isc/m1/trading_game/controller/Router.java
+++ src/main/java/fr/univ_smb/isc/m1/trading_game/controller/Router.java
@@ -1,6 +1,7 @@
 package fr.univ_smb.isc.m1.trading_game.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,7 +12,8 @@ public class Router {
     }
 
     @RequestMapping(value = "/gameManager")
-    public String gameManager() {
+    public String gameManager(Model model) {
+        //model.addAttribute("currentWallet", player.currentWallet.getBalance()); // TODO : mettre le vrai objet
         return "gameManager";
     }
 
