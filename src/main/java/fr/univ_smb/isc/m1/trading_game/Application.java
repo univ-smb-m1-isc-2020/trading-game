00fd65b1d3e2a250e8d5bea6cd37dd84af8f1e76
= <nils.ruet@etu.univ-smb.fr>
2021-04-20 09:40:21 +0200

    chore: enabled scheduling

--- src/main/java/fr/univ_smb/isc/m1/trading_game/Application.java
+++ src/main/java/fr/univ_smb/isc/m1/trading_game/Application.java
@@ -2,8 +2,10 @@ package fr.univ_smb.isc.m1.trading_game;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
