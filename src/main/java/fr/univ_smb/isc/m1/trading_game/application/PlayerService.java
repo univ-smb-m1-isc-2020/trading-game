a1c54e083180d9e74b60fd3cb73f66977ec8bb5b
= <nils.ruet@etu.univ-smb.fr>
2021-04-21 17:32:09 +0200

    feat: introduced player repository

--- src/main/java/fr/univ_smb/isc/m1/trading_game/application/PlayerService.java
+++ src/main/java/fr/univ_smb/isc/m1/trading_game/application/PlayerService.java
@@ -1,19 +1,17 @@
 package fr.univ_smb.isc.m1.trading_game.application;
 
-import fr.univ_smb.isc.m1.trading_game.infrastructure.persistence.EOD;
-import fr.univ_smb.isc.m1.trading_game.infrastructure.persistence.Order;
-import fr.univ_smb.isc.m1.trading_game.infrastructure.persistence.Player;
-import fr.univ_smb.isc.m1.trading_game.infrastructure.persistence.Portfolio;
+import fr.univ_smb.isc.m1.trading_game.infrastructure.persistence.*;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class PlayerService {
+    private PlayerRepository repository;
     private PortfolioService portfolioService;
 
     public void applyOrders(long playerId, EOD dayData){
-        Player player = new Player();//TODO get from repository
+        Player player = repository.getOne(playerId);
         List<Portfolio> portfolios = player.getPortfolios();
         for(Portfolio p : portfolios){
             portfolioService.applyOrders(p.getId(), dayData);
