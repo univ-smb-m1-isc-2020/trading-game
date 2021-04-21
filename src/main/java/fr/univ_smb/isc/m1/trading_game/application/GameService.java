93a41dade7489eb5028d68b3c1c0c2da22514d80
= <nils.ruet@etu.univ-smb.fr>
2021-04-21 17:34:02 +0200

    introduced game repository

--- src/main/java/fr/univ_smb/isc/m1/trading_game/application/GameService.java
+++ src/main/java/fr/univ_smb/isc/m1/trading_game/application/GameService.java
@@ -2,6 +2,7 @@ package fr.univ_smb.isc.m1.trading_game.application;
 
 import fr.univ_smb.isc.m1.trading_game.infrastructure.persistence.EOD;
 import fr.univ_smb.isc.m1.trading_game.infrastructure.persistence.Game;
+import fr.univ_smb.isc.m1.trading_game.infrastructure.persistence.GameRepository;
 import fr.univ_smb.isc.m1.trading_game.infrastructure.persistence.Player;
 import org.springframework.stereotype.Service;
 
@@ -10,22 +11,24 @@ import java.util.List;
 
 @Service
 public class GameService {
-
+    private GameRepository repository;
     private PlayerService playerService;
 
     public void addPlayer(long gameId, Player p){
-        Game game = new Game();//TODO repository
-        game.getPlayers().add(p);// TODO repo ?
+        Game game = repository.getOne(gameId);
+        game.getPlayers().add(p);
+        repository.save(game);
     }
 
-    public void applyDayData(){
+    public void applyDayData(long gameId){
         List<EOD> dayData = new ArrayList<>();//TODO EOD service
-        Game game = new Game();//TODO repository
+        Game game = repository.getOne(gameId);
         for(EOD eod : dayData){
             for(Player p : game.getPlayers()){
                 playerService.applyOrders(p.getId(),eod);
             }
         }
-        game.setCurrentDuration(game.getCurrentDuration()+1);;
+        game.setCurrentDuration(game.getCurrentDuration()+1);
+        repository.save(game);
     }
 }
