1656a2f000525a2a106b15e88d732ea57e7b7ed4
= <nils.ruet@etu.univ-smb.fr>
2021-04-22 14:14:22 +0200

    feat: completed TickerService (and fixed TickerRepository)

--- src/main/java/fr/univ_smb/isc/m1/trading_game/application/TickerService.java
+++ src/main/java/fr/univ_smb/isc/m1/trading_game/application/TickerService.java
@@ -1,20 +1,24 @@
 package fr.univ_smb.isc.m1.trading_game.application;
 
 import fr.univ_smb.isc.m1.trading_game.infrastructure.persistence.Ticker;
+import fr.univ_smb.isc.m1.trading_game.infrastructure.persistence.TickerRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
 
 @Service
 public class TickerService {
+    private final TickerRepository repository;
+
+    public TickerService(TickerRepository repository){
+        this.repository = repository;
+    }
+
     public List<Ticker> tickers(){
-        ArrayList<Ticker>  res = new ArrayList<>();//TODO get from repository
-        res.add(new Ticker());
-        res.add(new Ticker());
-        return res;
+        return repository.findAll();
     }
 
     public Ticker get(String mic){
-        return new Ticker(); // TODO get from repository
+        return repository.findById(mic).orElse(null);
     }
 }
--- src/main/java/fr/univ_smb/isc/m1/trading_game/infrastructure/persistence/TickerRepository.java
+++ src/main/java/fr/univ_smb/isc/m1/trading_game/infrastructure/persistence/TickerRepository.java
@@ -4,7 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TickerRepository extends JpaRepository<Ticker, Long> {
-
-    Ticker findByName(String name);
+public interface TickerRepository extends JpaRepository<Ticker, String> {
 }
