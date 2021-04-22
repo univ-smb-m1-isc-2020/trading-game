d40085cc67e1cd15f2a5d7bb45038a0e81bdf263
= <nils.ruet@etu.univ-smb.fr>
2021-04-22 10:22:57 +0200

    fix: fixed buyOrder applicable to the wrong ticker

--- src/main/java/fr/univ_smb/isc/m1/trading_game/application/BuyOrderService.java
+++ src/main/java/fr/univ_smb/isc/m1/trading_game/application/BuyOrderService.java
@@ -15,6 +15,9 @@ public class BuyOrderService {
 
     public boolean apply(long orderId, EOD dayData, long portfolioId) {
         BuyOrder order = repository.getOne(orderId);
+
+        if(isNotApplicable(order, dayData)) return false;
+
         int buyingPrice = dayData.getClose();
         int quantity = order.getQuantity();
         String symbol = dayData.getSymbol().getSymbol();
@@ -28,5 +31,9 @@ public class BuyOrderService {
         } else {
             return false;
         }
+    }
+
+    private boolean isNotApplicable(Order order, EOD dayData) {
+        return !order.isPending() || dayData.getSymbol() != order.getTicker();
     }
 }
