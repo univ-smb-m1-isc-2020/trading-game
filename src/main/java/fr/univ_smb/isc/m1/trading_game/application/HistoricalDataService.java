73a259499cfd405cd3f94e815f5505c94bdb7868
Camille <43339411+Camille0907@users.noreply.github.com>
2021-04-22 18:29:50 +0200

    feat: changed data scrapping to get eods for 60 tickers, on 330 days

--- src/main/java/fr/univ_smb/isc/m1/trading_game/application/HistoricalDataService.java
+++ src/main/java/fr/univ_smb/isc/m1/trading_game/application/HistoricalDataService.java
@@ -13,7 +13,9 @@ import org.springframework.web.client.RestTemplate;
 import javax.annotation.PostConstruct;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class HistoricalDataService {
@@ -27,40 +29,72 @@ public class HistoricalDataService {
 
     @PostConstruct
     public void initialize() {
+        String restResult;
+        Ticker currentTicker;
+        StringBuilder uri;
+        ObjectMapper mapper = new ObjectMapper();
+        RestTemplate restTemplate = new RestTemplate();
+
+        // Tickers initialization
         if (tickers().isEmpty()) {
+            JsonNode jTickers;
+            uri = new StringBuilder("http://api.marketstack.com/v1/exchanges/XPAR/tickers?" +
+                    "access_key=176434db214273ad3282785f999c7d42" +
+                    "&limit=60");
+
             try {
-                String restResult = new RestTemplate().getForEntity(new URI("http://api.marketstack.com/v1/exchanges/XPAR/tickers?access_key=176434db214273ad3282785f999c7d42"), String.class).getBody();
-                ObjectMapper mapper = new ObjectMapper();
-                JsonNode tickers = mapper.readTree(restResult).get("data").get("tickers");
-                for (final JsonNode jTicker : tickers) {
-                    Ticker ticker = mapper.treeToValue(jTicker, Ticker.class);
-                    tickerRepository.saveAndFlush(ticker);
+                // Getting 60 tickers
+                restResult = restTemplate.getForEntity(new URI(uri.toString()), String.class).getBody();
+                jTickers = mapper.readTree(restResult).get("data").get("tickers");
+
+                // Adding the tickers to db
+                for (final JsonNode jTicker : jTickers) {
+                    currentTicker = mapper.treeToValue(jTicker, Ticker.class);
+                    tickerRepository.saveAndFlush(currentTicker);
                 }
             } catch (URISyntaxException | JsonProcessingException e) {
                 e.printStackTrace();
             }
         }
 
+        // EODs initialization
         if (eods().isEmpty()) {
-            String uri;
-            String restResult;
-            ObjectMapper mapper = new ObjectMapper();
-            //for (final Ticker ticker : tickers()) {
-            Ticker ticker = tickers().get(1);
-            uri = "http://api.marketstack.com/v1/eod?access_key=176434db214273ad3282785f999c7d42&symbols=";
-            uri += ticker.symbol;
-            try {
-                restResult = new RestTemplate().getForEntity(new URI(uri), String.class).getBody();
-                JsonNode eods = mapper.readTree(restResult).get("data");
-                for (final JsonNode jEOD : eods) {
-                    EOD eod = mapper.treeToValue(jEOD, EOD.class);
-                    eod.symbol = ticker;
-                    eodRepository.saveAndFlush(eod);
+            Map<String, Ticker> currentTickers = new HashMap<>();
+            List<Ticker> tickers = tickers();
+            JsonNode jEODs;
+            EOD eod;
+
+            for (int i=0; i < tickers.size(); i+=3) {
+                uri = new StringBuilder("http://api.marketstack.com/v1/eod?" +
+                        "access_key=176434db214273ad3282785f999c7d42" +
+                        "&limit=1000" +
+                        "&date_from=2020-05-27" +
+                        "&date_to=2021-04-22" +
+                        "&symbols=");
+                //Working on three tickers at the same time to limit the number of queries
+                currentTickers.clear();
+               for (int j = i; j < i+3; j++) {
+                    try {
+                        currentTicker = tickers.get(j);
+                        currentTickers.put(currentTicker.symbol, currentTicker);
+                        uri.append(currentTicker.symbol);
+                        uri.append(",");
+                    } catch (IndexOutOfBoundsException ignored){} // Number of tickers wasn't a multiple of 3, that doesn't matter in the next
                 }
-            } catch (URISyntaxException | JsonProcessingException e) {
-                e.printStackTrace();
+                uri.deleteCharAt(uri.length()-1);
+                // Getting 330 latest eods for the 3 current tickers
+                try {
+                    restResult = restTemplate.getForEntity(new URI(uri.toString()), String.class).getBody();
+                    jEODs = mapper.readTree(restResult).get("data");
+                    for (final JsonNode jEOD : jEODs) {
+                        eod = mapper.treeToValue(jEOD, EOD.class);
+                        eod.symbol = currentTickers.get(jEOD.get("symbol").textValue());
+                        eodRepository.saveAndFlush(eod);
+                    }
+                } catch (URISyntaxException | JsonProcessingException e) {
+                    e.printStackTrace();
+                }
             }
-            //}
         }
     }
 
