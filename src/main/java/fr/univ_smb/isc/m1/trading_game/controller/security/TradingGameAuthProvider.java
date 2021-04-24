94e06bbfd127ccf91689dea31e090f0244dbf0a4
= <nils.ruet@etu.univ-smb.fr>
2021-04-24 10:03:26 +0200

    feat: added auth provider

---
+++ src/main/java/fr/univ_smb/isc/m1/trading_game/controller/security/TradingGameAuthProvider.java
@@ -0,0 +1,39 @@
+package fr.univ_smb.isc.m1.trading_game.controller.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TradingGameAuthProvider extends DaoAuthenticationProvider {
+
+    private final UserDetailsService userDetailsService;
+
+    public TradingGameAuthProvider(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    public Authentication authenticate(Authentication authentication) {
+        UsernamePasswordAuthenticationToken auth = (UsernamePasswordAuthenticationToken) authentication;
+        String name = auth.getName();
+        String password = auth.getCredentials().toString();
+        UserDetails user = userDetailsService.loadUserByUsername(name);
+        if (user == null) {
+            return null;
+        }
+        return new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return true;
+    }
+
+}
