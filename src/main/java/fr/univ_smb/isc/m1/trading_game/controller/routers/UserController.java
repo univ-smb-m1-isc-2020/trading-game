b0e323dd707da0643dd5ddcb4147bc7463ddf736
= <nils.ruet@etu.univ-smb.fr>
2021-04-25 17:18:54 +0200

    feat: bound the join game button in join game view

--- src/main/java/fr/univ_smb/isc/m1/trading_game/controller/routers/UserController.java
+++ src/main/java/fr/univ_smb/isc/m1/trading_game/controller/routers/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -72,8 +73,10 @@ public class UserController {
     }
 
     @PostMapping(value = URLMap.performJoinGame)
-    public String performJoinGame(){
-        //TODO
+    public String performJoinGame(@RequestParam(name = "gameId") long gameId){
+        TradingGameUser user = userService.getCurrentUser(SecurityContextHolder.getContext());
+        gameService.addPlayer(gameId, user);
+        //TODO redirect to game page
         return "redirect:"+URLMap.userHomepage;
     }
 
